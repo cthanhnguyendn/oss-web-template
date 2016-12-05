f113a1d324375ec4e2e7062017a31aec6100a257
cthanhnguyendn <cthanhnguyendn@gmail.com>
2016-12-05 14:56:17 +0700

    commit

--- documentation/docs.js
+++ documentation/docs.js
@@ -51,13 +51,4 @@ $(function () {
       }, 500);
     }
   });
-  //Skin switcher
-  var current_skin = "skin-blue";
-  $('#layout-skins-list [data-skin]').click(function(e) {
-    e.preventDefault();
-    var skinName = $(this).data('skin');
-    $('body').removeClass(current_skin);
-    $('body').addClass(skinName);
-    current_skin = skinName;
-  });
 });
